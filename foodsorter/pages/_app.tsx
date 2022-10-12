fd487e467a9d6712e1325cc68ff7d3b4d9fd8270
james glasgow <10838852+Glaschu@users.noreply.github.com>
2022-10-12 21:29:24 +0100

    Add Redux

--- foodsorter/pages/_app.tsx
+++ foodsorter/pages/_app.tsx
@@ -1,8 +1,15 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import React, {FC} from 'react';
+import {Provider} from 'react-redux';
+import type {AppProps} from 'next/app';
+import {wrapper} from '../components/store';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
-
+const MyApp: FC<AppProps> = ({Component, ...rest}) => {
+    const {store, props} = wrapper.useWrappedStore(rest);
+    return (
+        <Provider store={store}>
+            <Component {...props.pageProps} />
+        </Provider>
+    );
+};
 export default MyApp
